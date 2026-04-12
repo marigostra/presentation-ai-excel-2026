2664b675d634ee8fcf5a8dc28f17fc8b8e60064d
Michael Pozhidaev <msp@luwrain.org>
2026-04-12 09:58:19 +0700

    Adding -r to xargs to prevent running, if there are no *.uml files

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,7 +8,7 @@ pushd /etc/ImageMagick-* > /dev/null
 sudo sed -i -e s:'</policymap>':"  <policy domain="\""coder"\"" rights="\""read | write"\"" pattern="\""EPS"\"" />\n  <policy domain="\""coder"\"" rights="\""read | write"\"" pattern="\""PDF"\"" />\n</policymap>": policy.xml
 popd > /dev/null
 
-find -iname '*.uml' -print0 | xargs --null -n 1 java -jar plantuml.jar
+find -iname '*.uml' -print0 | xargs -r --null -n 1 java -jar plantuml.jar
 find -iname '*.png' | while read i; do
     PDF="${i/.png/.pdf}"
     echo "Converting $i to $PDF"    
